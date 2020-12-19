218a2e9cb4913ffc8fc57818c5c962eb68f8f5ce
zhanyeye <zhanyeye@qq.com>
2020-12-19 18:56:33 +0800

    Update EventManagerTest.java

--- src/test/java/cn/softeng/events/EventManagerTest.java
+++ src/test/java/cn/softeng/events/EventManagerTest.java
@@ -11,35 +11,85 @@ import org.junit.Test;
 @Slf4j
 public class EventManagerTest {
 
-    private static class TestTarget extends ProcessTarget {
-        final int num;
+    /**
+     * 测试仅从外部添加事件（即执行事件时不会触发新的事件），调度运行是否正常
+     * @throws InterruptedException
+     */
+    @Test
+    public void test_externalSchedule_noError() throws InterruptedException {
+        EventManager evt = new EventManager("DefaultEventManager");
+        log.debug("[----start----]");
+        for (int i = 0; i < 100; i++) {
+            evt.scheduleProcessExternal(i, 0, false, new ExternalTestTarget(i), null);
+        }
+        evt.resume(1000);
 
-        TestTarget(int num) {
+        // Junit本身是不支持普通的多线程测试的，这是因为Junit的底层实现上，是用System.exit退出用例执行的。
+        // JVM终止了，在测试线程启动的其他线程自然也无法执行。所以手动睡眠主线程。
+        while (true) {
+            Thread.sleep(1);
+        }
+    }
+
+    /**
+     * 用于测试外部调度任务的target (事件队列中的事件仅由外部添加)
+     */
+    private static class ExternalTestTarget extends ProcessTarget {
+        final int num;
+        ExternalTestTarget(int num) {
             this.num = num;
         }
 
         @Override
         public void process() {
-            log.debug("testTarget: " + num + " start run");
-            int currentTick = (int) Process.current().evt().getTicks();
+            log.debug("externalTestTarget: " + num + " start run");
             log.debug("currentTick is : " + Process.current().evt().getTicks());
         }
 
         @Override
         public String getDescription() {
-            return "Test" + num;
+            return "externalTest" + num;
         }
     }
 
+    /**
+     * 测试事件自身会产生新的事件时，运行是否正常
+     * @throws InterruptedException
+     */
     @Test
-    public void test_schedule_void() throws InterruptedException {
-        EventManager evt = new EventManager("DefaultEventManager");
-        log.debug("----start----");
-        for (int i = 0; i < 1000; i++) {
-            evt.scheduleProcessExternal(i, 0, false, new TestTarget(i), null);
-        }
+    public void test_internalSchedule_noError() throws InterruptedException {
+        EventManager evt = new EventManager("DefalutEventManager");
+        evt.scheduleProcessExternal(0, 0, false, new InternalTestTarget("External"), null);
         evt.resume(1000);
-        Thread.sleep(1000000);
+
+        // Junit本身是不支持普通的多线程测试的，这是因为Junit的底层实现上，是用System.exit退出用例执行的。
+        // JVM终止了，在测试线程启动的其他线程自然也无法执行。所以手动睡眠主线程。
+        while (true) {
+            Thread.sleep(1);
+        }
     }
+
+    /**
+     * 用于测试内部调度的target (事件自身在执行过程中可能会产生新的事件)
+     */
+    private static class InternalTestTarget extends ProcessTarget {
+        final String name;
+        InternalTestTarget(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void process() {
+           log.info("internalTestTarget" + name + "start run");
+           log.info("currentTick is " + Process.current().evt().getTicks());
+           EventManager.scheduleTicks(2, 0, false, new InternalTestTarget("createbyinternal"), null);
+        }
+
+        @Override
+        public String getDescription() {
+            return "internalTest " + name;
+        }
+    }
+
 
 }
