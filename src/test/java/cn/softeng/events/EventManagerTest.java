e0f3e39d9d4af926349c243880eac81e678735cd
zhanyeye <zhanyeye@qq.com>
2020-12-19 16:36:17 +0800

    add lombok, log4j2 and slf4j

--- src/test/java/cn/softeng/events/EventManagerTest.java
+++ src/test/java/cn/softeng/events/EventManagerTest.java
@@ -1,68 +1,45 @@
 package cn.softeng.events;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
+ * EventManager测试类，主要测试事件调度机制是否正常
  * @date: 12/14/2020 4:08 PM
  */
+@Slf4j
 public class EventManagerTest {
-    @Test
-    public void test_func_null() {
-        EventManager evt = new EventManager("DefaultEventManager");
 
-
-    }
-
-
-    @Test
-    public void testScheduleTime() {
-        EventManager evt = new EventManager("TestScheduleTimeEVT");
-        evt.clear();
-
-        ArrayList<String> log = new ArrayList<>();
-        evt.scheduleProcessExternal(0, 0, false, new LogTarget(0, log), null);
-        evt.scheduleProcessExternal(1, 0, false, new LogTarget(1, log), null);
-        evt.scheduleProcessExternal(2, 0, false, new LogTarget(2, log), null);
-        evt.scheduleProcessExternal(3, 0, false, new LogTarget(3, log), null);
-        evt.scheduleProcessExternal(4, 0, false, new LogTarget(4, log), null);
-
-        TestFrameworkHelpers.runEventsToTick(evt, 100, 1000);
-
-        ArrayList<String> expected = new ArrayList<>();
-        expected.add("Target:0");
-        expected.add("Target:1");
-        expected.add("Target:2");
-        expected.add("Target:3");
-        expected.add("Target:4");
-
-        assertTrue(expected.size() == log.size());
-        for (int i = 0; i < expected.size(); i++) {
-            assertTrue(expected.get(i).equals(log.get(i)));
-        }
-    }
-
-    private static class LogTarget extends ProcessTarget {
-        final ArrayList<String> log;
+    private static class TestTarget extends ProcessTarget {
         final int num;
-        LogTarget(int i, ArrayList<String> l) {
-            log = l;
-            num = i;
-        }
 
-        @Override
-        public String getDescription() {
-            return "Target:" + num;
+        TestTarget(int num) {
+            this.num = num;
         }
 
         @Override
         public void process() {
-            log.add("Target:" + num);
+            log.debug("testTarget: " + num + " start run");
+            int currentTick = (int) Process.current().evt().getTicks();
+            log.debug("currentTick is : " + Process.current().evt().getTicks());
         }
+
+        @Override
+        public String getDescription() {
+            return "Test" + num;
+        }
+    }
+
+    @Test
+    public void test_schedule_void() throws InterruptedException {
+        EventManager evt = new EventManager("DefaultEventManager");
+        log.debug("----start----");
+        for (int i = 0; i < 1000; i++) {
+            evt.scheduleProcessExternal(i, 0, false, new TestTarget(i), null);
+        }
+        evt.resume(1000);
+        Thread.sleep(1000000);
     }
 
 }
