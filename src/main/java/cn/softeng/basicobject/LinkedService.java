46b6c77e068018a9169737ca30ce7d47d9567bb4
zhanyeye <zhanyeye@qq.com>
2020-12-21 09:04:31 +0800

    Update LinkedService.java

--- src/main/java/cn/softeng/basicobject/LinkedService.java
+++ src/main/java/cn/softeng/basicobject/LinkedService.java
@@ -1,8 +1,19 @@
 package cn.softeng.basicobject;
 
+import java.util.ArrayList;
+
 /**
  * @date: 12/17/2020 10:00 AM
  */
-public class LinkedService {
+public class LinkedService extends LinkedComponent implements QueueUser {
 
+    @Override
+    public ArrayList<Queue> getQueues() {
+        return null;
+    }
+
+    @Override
+    public void queueChanged() {
+
+    }
 }
