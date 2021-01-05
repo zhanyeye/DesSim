9d22ff2f63f160cbf787fdc56b2ff9c674d98c90
zhanyeye <zhanyeye@qq.com>
2021-01-05 16:29:44 +0800

    Update DesSimTest.java

--- src/test/java/cn/softeng/DesSimTest.java
+++ src/test/java/cn/softeng/DesSimTest.java
@@ -47,7 +47,7 @@ public class DesSimTest {
 
         DesSim.inject(0, 1);
 
-        DesSim.doEvent(5);
+        DesSim.doEvent(0);
         DesSim.doEvent(7);
 
         log.debug("{}", DesSim.hasEvent() ? "has Event" : "no Event");
@@ -60,7 +60,7 @@ public class DesSimTest {
 
         DesSim.inject(15, 1);
 
-        DesSim.doEvent(100);
+        DesSim.doEvent(50);
 
         log.debug("{}", DesSim.hasEvent() ? "has Event" : "no Event");
 
@@ -104,25 +104,25 @@ public class DesSimTest {
         generator.setNextComponent(queue1);
         generator.setEntitiesPerArrival(1);
         generator.setFirstArrivalTime(0);
-        generator.setInterArrivalTime(1);
+        generator.setInterArrivalTime(5);
 
 
         server1.setWaitQueue(queue1);
-        server1.setServiceTime(2);
+        server1.setServiceTime(5);
         server1.setNextComponent(queue2);
 
         server2.setWaitQueue(queue2);
-        server2.setServiceTime(4);
+        server2.setServiceTime(5);
         server2.setNextComponent(sink);
 
         // ********************************
         // 运行模型
         // ********************************
         DesSim.initModel(DesSim.Type.Generator);
-        DesSim.resume(0);
-        DesSim.resume(0);
-        log.debug("{}", DesSim.hasEvent());
-        log.debug("{}", DesSim.minEventTime());
+
+        DesSim.resume(50);
+        log.debug("hasEvent:{}", DesSim.hasEvent());
+        log.debug("minEventTime:{}", DesSim.minEventTime());
 
         // *******************************
         // 获取数据
@@ -130,7 +130,7 @@ public class DesSimTest {
 
         // 输出时钟序列
         // 输出时钟序列
-        log.debug("{}", DesSim.getEntity(3).getName());
+        log.debug("{}", DesSim.getEntity(3).getClass());
         log.debug("{}", DesSim.getDesCLockList().toString());
         log.debug("{}", DesSim.getDataList(3, DesSim.NumberAdded).toString());
         log.debug("{}", DesSim.getDataList(3, DesSim.NumberProcessed).toString());
