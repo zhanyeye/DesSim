1982a822b84dd779585e956c37ac2e93474753e4
zhanyeye <zhanyeye@qq.com>
2021-01-31 19:18:05 +0800

    update ProcessTarget

--- src/main/java/cn/softeng/basicsim/ClonesOfIterable.java
+++ src/main/java/cn/softeng/basicsim/ClonesOfIterable.java
@@ -5,6 +5,7 @@ package cn.softeng.basicsim;
  * @date: 12/17/2020 2:53 PM
  */
 public class ClonesOfIterable<T extends Entity> extends EntityIterator<T> {
+
     public ClonesOfIterable(Class<T> tClass) {
         super(tClass);
     }
--- src/main/java/cn/softeng/basicsim/InstanceIterable.java
+++ src/main/java/cn/softeng/basicsim/InstanceIterable.java
@@ -5,6 +5,7 @@ package cn.softeng.basicsim;
  * @date: 12/17/2020 2:08 PM
  */
 public class InstanceIterable<T extends Entity> extends EntityIterator<T> {
+
     public InstanceIterable(Class<T> tClass) {
         super(tClass);
     }
