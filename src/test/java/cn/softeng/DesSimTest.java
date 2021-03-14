ac2896c766c71a837f9e99a3601efb7bcd62e5cd
zhanyeye <zhanyeye@qq.com>
2021-03-14 10:24:04 +0800

    Update DesSimTest.java

--- src/test/java/cn/softeng/DesSimTest.java
+++ src/test/java/cn/softeng/DesSimTest.java
@@ -13,164 +13,157 @@ import java.util.Map;
 @Slf4j
 public class DesSimTest {
 
-        @Test
-        public void testGeneratorScheduling() {
+    @Test
+    public void testGeneratorScheduling() {
 
-                // ************************************************
-                // 定义模型, 同时设置标识符，(先定义出所有组件，在给组件赋值)
-                // ************************************************
+        // ************************************************
+        // 定义模型, 同时设置标识符，(先定义出所有组件，在给组件赋值)
+        // ************************************************
 
-                EntityGenerator generator = new EntityGenerator("EntityGenerator");
-                SimEntity simEntity = new SimEntity("DefaultEntity");
-                Assign assign = new Assign("assign");
-                Queue queue1 = new Queue("Queue1");
-                Queue queue2 = new Queue("Queue2");
-                Server server1 = new Server("Server1");
-                Server server2 = new Server("Server2");
-                EntitySink sink = new EntitySink("EntitySink");
+        EntityGenerator generator = new EntityGenerator("EntityGenerator");
+        SimEntity simEntity = new SimEntity("DefaultEntity");
+        Assign assign = new Assign("assign");
+        Queue queue1 = new Queue("Queue1");
+        Queue queue2 = new Queue("Queue2");
+        Server server1 = new Server("Server1");
+        Server server2 = new Server("Server2");
+        EntitySink sink = new EntitySink("EntitySink");
 
-                // ******************************
-                // 为模型属性赋值
-                // ******************************
+        // ******************************
+        // 为模型属性赋值
+        // ******************************
 
-                generator.setNextComponent(assign);
-                generator.setEntitiesPerArrival(1);
-                generator.setFirstArrivalTime(7);
-                generator.setInterArrivalTime(7);
-                generator.setPrototypeEntity(simEntity);
+        generator.setNextComponent(assign);
+        generator.setEntitiesPerArrival(1);
+        generator.setFirstArrivalTime(7);
+        generator.setInterArrivalTime(7);
+        generator.setPrototypeEntity(simEntity);
 
-                Map<String, Integer> map = new HashMap<>();
-                map.put("red", 3);
-                map.put("black", 7);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("red", 3);
+        map.put("black", 7);
 
-                assign.addAssignment(map);
-                assign.setNextComponent(queue1);
+        assign.addAssignment(map);
+        assign.setNextComponent(queue1);
 
-                Map<String, Double> map1 = new HashMap<>();
-                map1.put("red", 10.0);
-                map1.put("black", 15.0);
-                server1.setServerTimeChoice(map1);
-                server1.setWaitQueue(queue1);
-                server1.setServiceTime(5);
-                server1.setNextComponent(queue2);
+        Map<String, Double> map1 = new HashMap<>();
+        map1.put("red", 10.0);
+        map1.put("black", 15.0);
+        server1.setServerTimeChoice(map1);
+        server1.setWaitQueue(queue1);
+        server1.setServiceTime(5);
+        server1.setNextComponent(queue2);
 
-                server2.setWaitQueue(queue2);
-                server2.setServiceTime(5);
-                server2.setNextComponent(sink);
+        server2.setWaitQueue(queue2);
+        server2.setServiceTime(5);
+        server2.setNextComponent(sink);
 
-                // ********************************
-                // 运行模型
-                // ********************************
+        // ********************************
+        // 运行模型
+        // ********************************
 
-                // 初始化模型
-                DesSim.initModel(DesSim.Type.Generator, 0);
+        // 初始化模型
+        DesSim.initModel(DesSim.Type.Generator, 0);
 
-                server2.setWaitQueue(queue2);
-                server2.setServiceTime(5);
-                server2.setNextComponent(sink);
+        server2.setWaitQueue(queue2);
+        server2.setServiceTime(5);
+        server2.setNextComponent(sink);
 
-                // 仿真时钟推进到 50时刻
-                DesSim.resume(6000);
+        // 仿真时钟推进到 50时刻
+        DesSim.resume(6000);
 
-                // 仿真时钟推进到 50时刻
-                DesSim.resume(16.4588);
+        // 仿真时钟推进到 50时刻
+        DesSim.resume(16.4588);
 
-                // 事件队列中是否有事件
-                log.debug("hasEvent:{}", DesSim.hasEvent());
-                // 事件队列中最近事件的时间
-                log.debug("minEventTime:{}", DesSim.minEventTime());
+        // 事件队列中是否有事件
+        log.debug("hasEvent:{}", DesSim.hasEvent());
+        // 事件队列中最近事件的时间
+        log.debug("minEventTime:{}", DesSim.minEventTime());
 
-                // *******************************
-                // 获取统计数据
-                // *******************************
+        // *******************************
+        // 获取统计数据
+        // *******************************
 
-                log.debug("{}", DesSim.getEntity("Server1").getClass());
-                log.debug("{}", DesSim.getDesClockList().toString());
-                log.debug("{}", DesSim.getDataList("Server1", DesSim.NumberAdded).toString());
-                log.debug("{}", DesSim.getDataList("Server1", DesSim.NumberProcessed).toString());
-                log.debug("{}", DesSim.getDataList("Server1", DesSim.NumberInProgress).toString());
+        log.debug("{}", DesSim.getEntity("Server1").getClass());
+        log.debug("{}", DesSim.getDesClockList().toString());
+        log.debug("{}", DesSim.getDataList("Server1", DesSim.NumberAdded).toString());
+        log.debug("{}", DesSim.getDataList("Server1", DesSim.NumberProcessed).toString());
+        log.debug("{}", DesSim.getDataList("Server1", DesSim.NumberInProgress).toString());
 
-        }
+    }
 
-        @Test
-        public void testLauncherScheduling() {
+    @Test
+    public void testLauncherScheduling() {
+            
 
-                EntityLauncher launcher = new EntityLauncher("launcher");
+        EntityLauncher launcher = new EntityLauncher("launcher");
+        Queue queue1 = new Queue("queue1");
+        Queue queue2 = new Queue("queue2");
+        Server server1 = new Server("server1");
+        Server server2 = new Server("server2");
+        EntitySink sink = new EntitySink("sink");
 
-                Queue queue1 = new Queue("queue1");
-                Queue queue2 = new Queue("queue2");
-                Server server1 = new Server("server1");
-                Server server2 = new Server("server2");
-                EntitySink sink = new EntitySink("sink");
+        // ******************************
+        // 为模型属性赋值
+        // ******************************
 
-                EntityLauncher launcher = new EntityLauncher("launcher");
-                Queue queue1 = new Queue("queue1");
-                Queue queue2 = new Queue("queue2");
-                Server server1 = new Server("server1");
-                Server server2 = new Server("server2");
-                EntitySink sink = new EntitySink("sink");
+        launcher.setNextComponent(queue1);
+        server1.setWaitQueue(queue1);
+        server1.setServiceTime(5);
+        server1.setNextComponent(queue2);
+        server2.setWaitQueue(queue2);
+        server2.setServiceTime(5);
+        server2.setNextComponent(sink);
 
-                // ******************************
-                // 为模型属性赋值
-                // ******************************
+        // ********************************
+        // 运行模型
+        // ********************************
 
-                launcher.setNextComponent(queue1);
-                server1.setWaitQueue(queue1);
-                server1.setServiceTime(5);
-                server1.setNextComponent(queue2);
-                server2.setWaitQueue(queue2);
-                server2.setServiceTime(5);
-                server2.setNextComponent(sink);
+        DesSim.initModel(DesSim.Type.Launcher);
 
-                // ********************************
-                // 运行模型
-                // ********************************
+        log.debug("hasEvent:{}", DesSim.hasEvent());
+        // 下一个事件的发生时间
+        log.debug("nextEventTime:{}", DesSim.minEventTime());
 
-                DesSim.initModel(DesSim.Type.Launcher);
+        DesSim.inject(0, 1);
 
-                log.debug("hasEvent:{}", DesSim.hasEvent());
-                // 下一个事件的发生时间
-                log.debug("nextEventTime:{}", DesSim.minEventTime());
+        log.debug("hasEvent:{}", DesSim.hasEvent());
+        // 下一个事件的发生时间
+        log.debug("nextEventTime:{}", DesSim.minEventTime());
 
-                DesSim.inject(0, 1);
+        // 仿真时钟推进到 5时刻
+        DesSim.resume(5);
+        // 仿真时钟推进到 7时刻
+        DesSim.resume(7);
 
-                log.debug("hasEvent:{}", DesSim.hasEvent());
-                // 下一个事件的发生时间
-                log.debug("nextEventTime:{}", DesSim.minEventTime());
+        log.debug("{}", DesSim.hasEvent() ? "has Event" : "no Event");
+        log.debug("{}", DesSim.minEventTime());
 
-                // 仿真时钟推进到 5时刻
-                DesSim.resume(5);
-                // 仿真时钟推进到 7时刻
-                DesSim.resume(7);
+        DesSim.inject(7, 1);
 
-                log.debug("{}", DesSim.hasEvent() ? "has Event" : "no Event");
-                log.debug("{}", DesSim.minEventTime());
+        // 仿真时钟推进到 10时刻
+        DesSim.resume(10);
+        // 仿真时钟推进到 15时刻
+        DesSim.resume(15);
 
-                DesSim.inject(7, 1);
+        DesSim.inject(15, 1);
+        // 仿真时钟推进到 50时刻
+        DesSim.resume(16.4588);
 
-                // 仿真时钟推进到 10时刻
-                DesSim.resume(10);
-                // 仿真时钟推进到 15时刻
-                DesSim.resume(15);
+        log.debug("{}", DesSim.hasEvent() ? "has Event" : "no Event");
+        log.debug("{}", DesSim.minEventTime());
 
-                DesSim.inject(15, 1);
-                // 仿真时钟推进到 50时刻
-                DesSim.resume(16.4588);
+        // *******************************
+        // 获取统计数据
+        // *******************************
 
-                log.debug("{}", DesSim.hasEvent() ? "has Event" : "no Event");
-                log.debug("{}", DesSim.minEventTime());
+        // 输出时钟序列
+        log.debug("Server:");
+        log.debug("{}", DesSim.getDesClockList().toString());
+        log.debug("{}", server1.getNumAddList().toString());
+        log.debug("{}", server1.getNumProcessedList().toString());
+        log.debug("{}", server1.getNumInProgressList().toString());
 
-                // *******************************
-                // 获取统计数据
-                // *******************************
-
-                // 输出时钟序列
-                log.debug("Server:");
-                log.debug("{}", DesSim.getDesClockList().toString());
-                log.debug("{}", server1.getNumAddList().toString());
-                log.debug("{}", server1.getNumProcessedList().toString());
-                log.debug("{}", server1.getNumInProgressList().toString());
-
-        }
+    }
 
 }
